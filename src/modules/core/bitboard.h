dff3b5e643c3526585af0f0667f56f73ff1750ee
Gaspard <adr.gaspard@gmail.com>
2023-07-17 15:48:57 +0200

    Small fixes

--- src/modules/core/bitboard.h
+++ src/modules/core/bitboard.h
@@ -3,33 +3,33 @@
 
 #include "types.h"
 
-static inline bitboard set_position_to_zero(bitboard bitboard, position position);
-static inline bitboard set_position_to_one(bitboard bitboard, position position);
-static inline bitboard toggle_position(bitboard bitboard, position position);
-static inline bitboard toggle_positions(bitboard bitboard, position first, position second);
-static inline bitboard contains_position(bitboard bitboard, position position);
+static inline void set_position_to_zero(bitboard bitboard, position position);
+static inline void set_position_to_one(bitboard bitboard, position position);
+static inline void toggle_position(bitboard bitboard, position position);
+static inline void toggle_positions(bitboard bitboard, position first, position second);
+static inline bool contains_position(bitboard bitboard, position position);
 
-static inline bitboard set_position_to_zero(bitboard bitboard, position position)
+static inline void set_position_to_zero(bitboard *bitboard, position position)
 {
-    return bitboard | (1ULL << position);
+    *bitboard |= (1ULL << position);
 }
 
-static inline bitboard set_position_to_one(bitboard bitboard, position position)
+static inline void set_position_to_one(bitboard *bitboard, position position)
 {
-    return bitboard & ~(1ULL << position);
+    *bitboard &= ~(1ULL << position);
 }
 
-static inline bitboard toggle_position(bitboard bitboard, position position)
+static inline void toggle_position(bitboard *bitboard, position position)
 {
-    return bitboard ^ (1ULL << position);
+    *bitboard ^= (1ULL << position);
 }
 
-static inline bitboard toggle_positions(bitboard bitboard, position first, position second)
+static inline void toggle_positions(bitboard *bitboard, position first, position second)
 {
-    return bitboard ^ (1ULL << first | 1ULL << second);
+    *bitboard ^= (1ULL << first | 1ULL << second);
 }
 
-static inline bitboard contains_position(bitboard bitboard, position position)
+static inline bool contains_position(bitboard bitboard, position position)
 {
     return ((bitboard >> position) & 1) != 0;
 }
