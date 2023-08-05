265e46782662632d0c6a5f29c578614f0b1a2722
Gaspard <adr.gaspard@gmail.com>
2023-08-05 19:52:41 +0200

    Add magic bitboards getters

--- src/modules/core/precomputed_board_data.h
+++ src/modules/core/precomputed_board_data.h
@@ -19,5 +19,31 @@ void initialize_precomputed_board_data();
 bitboard_dynamic_array compute_blockers_bitboards(bitboard moves_mask);
 bitboard compute_moves_mask(position start_pos, bool ortho_instead_of_diag);
 bitboard compute_legal_moves_mask(position start_pos, bitboard blockers_bitboard, bool ortho_instead_of_diag);
+static inline bitboard get_slider_moves_bitboard(position position, bitboard blockers, bool ortho_instead_of_diag);
+static inline bitboard get_orthogonal_moves_bitboard(position position, bitboard blockers);
+static inline bitboard get_diagonal_moves_bitboard(position position, bitboard blockers);
+
+static inline bitboard get_slider_moves_bitboard(position position, bitboard blockers, bool ortho_instead_of_diag)
+{
+    return ortho_instead_of_diag ? get_orthogonal_moves_bitboard(position, blockers) : get_diagonal_moves_bitboard(position, blockers);
+}
+
+static inline bitboard get_orthogonal_moves_bitboard(position position, bitboard blockers)
+{
+    bitboard key;
+    magic_data *data;
+    data = &(g_orthogonal_magic_data[position]);
+    key = ((blockers & g_orthogonal_moves_mask[position]) * data->value) >> data->shift_quantity;
+    return data->legal_moves[key];
+}
+
+static inline bitboard get_diagonal_moves_bitboard(position position, bitboard blockers)
+{
+    bitboard key;
+    magic_data *data;
+    data = &(g_diagonal_magic_data[position]);
+    key = ((blockers & g_diagonal_moves_mask[position]) * data->value) >> data->shift_quantity;
+    return data->legal_moves[key];
+}
 
 #endif // PRECOMPUTED_BOARD_DATA_H
