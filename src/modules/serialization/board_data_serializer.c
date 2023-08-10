80a0f9ae352e91a7caf276907926d996867b591d
Gaspard <adr.gaspard@gmail.com>
2023-08-10 23:42:04 +0200

    Add board/board_data convertion

--- src/modules/serialization/board_data_serializer.c
+++ src/modules/serialization/board_data_serializer.c
@@ -1,3 +1,4 @@
+#include "../core/board.h"
 #include "board_data_serializer.h"
 
 board_data board_data_from_fen_string(char *fen_string)
@@ -19,17 +20,58 @@ void board_data_to_fen_string(board_data board_data, char *result)
 
 void load_board_from_board_data(board *board, board_data board_data)
 {
+    position pos;
+    piece piece;
+    color piece_color;
+    piece_type piece_type;
+    zobrist_key key;
     assert(board);
-    (void)board;
-    (void)board_data;
-    // TODO
+    for (pos = 0; pos < POSITIONS_COUNT; pos++)
+    {
+        piece = board_data.position[pos];
+        board->position[pos] = piece;
+        if (!is_piece_empty(piece))
+        {
+            piece_color = get_color(piece);
+            piece_type = get_type(piece);
+            set_position_to_one(&(board->color_mask[piece_color]), pos);
+            if (piece_type == PIECE_KING)
+            {
+                board->king_position[piece_color] = pos;
+            }
+            else
+            {
+                set_position_to_one(&(board->piece_masks[piece_color][piece_type]), pos);
+                add_position_to_group(&(board->piece_groups[piece_color][piece_type]), pos);
+            }
+            if (piece_type != PIECE_PAWN && piece_type != PIECE_KING)
+            {
+                board->special_piece_count++;
+            }
+        }
+    }
+    board->color_to_move = board_data.color_to_move;
+    board->all_piece_mask = board->color_mask[COLOR_WHITE] | board->color_mask[COLOR_BLACK];
+    update_sliders(board);
+    board->current_game_state = create_game_state(board_data.castling_data, board_data.last_en_passant_file, board_data.half_move_count, create_empty_piece(), 0);
+    key = compute_zobrist_key(board);
+    board->current_game_state = create_game_state(board_data.castling_data, board_data.last_en_passant_file, board_data.half_move_count, create_empty_piece(), key);
+    push_on_zobrist_stack(board->position_repetition_history, key);
+    push_on_game_state_stack(board->game_state_history, board->current_game_state);
 }
 
 board_data board_to_board_data(board *board)
 {
     board_data board_data;
+    position pos;
     assert(board);
-    (void)board;
-    // TODO
+    for (pos = 0; pos < POSITIONS_COUNT; pos++)
+    {
+        board_data.position[pos] = board->position[pos];
+    }
+    board_data.color_to_move = board->color_to_move;
+    board_data.castling_data = board->current_game_state.castling_data;
+    board_data.last_en_passant_file = board->current_game_state.last_en_passant_file;
+    board_data.half_move_count = board->current_game_state.half_move_count;
     return board_data;
 }
