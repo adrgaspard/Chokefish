17fd72bec11bf32e7452e8f3249cbf709f30ccfa
Gaspard <adr.gaspard@gmail.com>
2023-08-14 20:26:33 +0200

    Add some UCI commands handling

--- src/modules/uci/engine_state.h
+++ src/modules/uci/engine_state.h
@@ -7,12 +7,15 @@ typedef enum engine_state
 {
     WAITING_FOR_SETUP,
     WAITING_FOR_READY,
+    IDLING,
 } engine_state;
 
 
 static inline engine_state get_default_state();
 static inline bool is_waiting_for_setup(engine_state engine_state);
+static inline bool is_waiting_for_ready(engine_state engine_state);
 static inline void on_sending_uciok(engine_state *engine_state);
+static inline void on_sending_readyok(engine_state *engine_state);
 
 static inline engine_state get_default_state()
 {
@@ -24,10 +27,24 @@ static inline bool is_waiting_for_setup(engine_state engine_state)
     return engine_state == WAITING_FOR_SETUP;
 }
 
+static inline bool is_waiting_for_ready(engine_state engine_state)
+{
+    return engine_state == WAITING_FOR_READY;
+}
+
 static inline void on_sending_uciok(engine_state *engine_state)
 {
     assert(is_waiting_for_setup(*engine_state));
     *engine_state = WAITING_FOR_READY;
+}
+
+static inline void on_sending_readyok(engine_state *engine_state)
+{
+    assert(!is_waiting_for_setup(*engine_state));
+    if (is_waiting_for_ready(*engine_state))
+    {
+        *engine_state = IDLING;
+    }
 }
 
 #endif // ENGINE_STATE_H
--- src/modules/uci/uci.c
+++ src/modules/uci/uci.c
@@ -10,66 +10,72 @@
 static char s_command_str[COMMAND_STR_LEN_WITH_ENDCHAR] = "";
 static engine_options s_options;
 static engine_state s_state;
+static bool s_debug;
 
 static void handle_uci_command();
+static void handle_debug_command(char *current_cmd);
+static void handle_isready_command();
+static void handle_ucinewgame_command();
+static void handle_quit_command();
 
 void handle_commands()
 {
-    char *current_arg;
+    char *current_cmd;
     s_state = get_default_state();
     s_options = get_default_options();
+    s_debug = false;
     while (true)
     {
         if (fgets(s_command_str, COMMAND_STR_LEN, stdin) == NULL)
         {
             break;
         }
-        current_arg = strtok(s_command_str, UCI_DELIMITER);
-        while (current_arg != NULL)
+        current_cmd = strtok(s_command_str, UCI_DELIMITER);
+        while (current_cmd != NULL)
         {
-            if (strcmp(current_arg, GE_CMD_UCI) == 0)
+            if (strcmp(current_cmd, GE_CMD_UCI) == 0)
             {
                 handle_uci_command();
             }
-            else if (strcmp(current_arg, GE_CMD_DEBUG) == 0)
+            else if (strcmp(current_cmd, GE_CMD_DEBUG) == 0)
+            {
+                handle_debug_command(current_cmd);
+            }
+            else if (strcmp(current_cmd, GE_CMD_ISREADY) == 0)
+            {
+                handle_isready_command();
+            }
+            else if (strcmp(current_cmd, GE_CMD_SETOPTION) == 0)
             {
 
             }
-            else if (strcmp(current_arg, GE_CMD_ISREADY) == 0)
+            else if (strcmp(current_cmd, GE_CMD_UCINEWGAME) == 0)
+            {
+                handle_ucinewgame_command();
+            }
+            else if (strcmp(current_cmd, GE_CMD_POSITION) == 0)
             {
 
             }
-            else if (strcmp(current_arg, GE_CMD_SETOPTION) == 0)
+            else if (strcmp(current_cmd, GE_CMD_GO) == 0)
             {
 
             }
-            else if (strcmp(current_arg, GE_CMD_UCINEWGAME) == 0)
+            else if (strcmp(current_cmd, GE_CMD_STOP) == 0)
             {
 
             }
-            else if (strcmp(current_arg, GE_CMD_POSITION) == 0)
+            else if (strcmp(current_cmd, GE_CMD_PONDERHIT) == 0)
             {
 
             }
-            else if (strcmp(current_arg, GE_CMD_GO) == 0)
+            else if (strcmp(current_cmd, GE_CMD_QUIT) == 0)
             {
-
-            }
-            else if (strcmp(current_arg, GE_CMD_STOP) == 0)
-            {
-
-            }
-            else if (strcmp(current_arg, GE_CMD_PONDERHIT) == 0)
-            {
-
-            }
-            else if (strcmp(current_arg, GE_CMD_QUIT) == 0)
-            {
-
+                handle_quit_command();
             }
             else
             {
-                current_arg = strtok(NULL, UCI_DELIMITER);
+                current_cmd = strtok(NULL, UCI_DELIMITER);
             }
         }
     }
@@ -90,4 +96,43 @@ static void handle_uci_command()
         on_sending_uciok(&s_state);
         printf(EG_CMD_UCIOK "\n");
     }
+}
+
+static void handle_debug_command(char *current_cmd)
+{  
+    current_cmd = strtok(NULL, UCI_DELIMITER);
+    if (current_cmd != NULL)
+    {
+        if (strcmp(current_cmd, GE_CMD_DEBUG_OPT_ON) == 0)
+        {
+            s_debug = true;
+        }
+        else if (strcmp(current_cmd, GE_CMD_DEBUG_OPT_OFF) == 0)
+        {
+            s_debug = false;
+        }
+    }
+}
+
+static void handle_isready_command()
+{
+    if (!is_waiting_for_setup(s_state))
+    {
+        on_sending_readyok(&s_state);
+        printf(EG_CMD_READYOK "\n");
+    }
+}
+
+static void handle_ucinewgame_command()
+{
+    if (!is_waiting_for_setup(s_state))
+    {
+        // TODO : if searching: stop the search, don't return anything
+        // TODO : clear all search cache, put the state on idling
+    }
+}
+
+static void handle_quit_command()
+{
+    exit(EXIT_SUCCESS);
 }
