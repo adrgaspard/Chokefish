1d4ee4ccc9e6bb1c1da424518610fd2a05363a8f
Gaspard Andrieu <adr.gaspard@gmail.com>
2023-09-06 16:32:29 +0200

    Bug fix & code simplification

--- engine/src/modules/uci/command_position.c
+++ engine/src/modules/uci/command_position.c
@@ -16,12 +16,11 @@ move_generation_options s_all_moves = { .include_quiet_moves = true, .promotion_
 
 void handle_position_command(char *cmd, char *edit_cmd, uint64_t start_index, engine_state *state, board *board, search_token *token, char *current_fen, char *current_moves)
 {
-    char fen[FEN_LENGTH_UPPER_BOUND + 1], *command_args, *moves_ptr, *fen_ptr, *new_moves_ptr, *current_move_ptr;
-    uint64_t moves_opt_len, fen_part_len, fen_opt_static_len, uci_delimiter_static_len, new_moves_index;
+    char fen[FEN_LENGTH_UPPER_BOUND + 1], *command_args, *moves_ptr, *fen_ptr, *current_move_ptr;
+    uint64_t moves_opt_len, fen_part_len, fen_opt_static_len;
     move_generation_result generation_result;
     move current_move;
     move_data move_data;
-    bool is_new_game;
     assert(cmd != NULL);
     assert(edit_cmd != NULL);
     assert(state != NULL);
@@ -38,12 +37,9 @@ void handle_position_command(char *cmd, char *edit_cmd, uint64_t start_index, en
     {
         return;
     }
-    uci_delimiter_static_len = strlen(UCI_DELIMITER);
     fen_opt_static_len = strlen(GE_CMD_POSITION_OPT_FEN UCI_DELIMITER);
     moves_ptr = NULL;
     moves_opt_len = 0;
-    new_moves_index = 0;
-    new_moves_ptr = NULL;
     command_args = edit_cmd + start_index;
     if (strstr(command_args, GE_CMD_POSITION_OPT_MOVES) != NULL)
     {
@@ -74,61 +70,9 @@ void handle_position_command(char *cmd, char *edit_cmd, uint64_t start_index, en
         strncpy(fen, fen_ptr, fen_part_len - fen_opt_static_len);
         fen[fen_part_len - fen_opt_static_len] = '\0';
     }
-    if (strcmp(current_fen, fen) != 0)
+    if (moves_ptr != NULL)
     {
-        is_new_game = true;
-        strcpy(current_fen, fen);
-        strcpy(current_moves, moves_ptr == NULL ? "" : moves_ptr);
-    }
-    else if (moves_ptr == NULL)
-    {
-        is_new_game = true;
-    }
-    else
-    {
-        while (true)
-        {
-            if (moves_ptr[new_moves_index] == current_moves[new_moves_index])
-            {
-                if (current_moves[new_moves_index] == '\0')
-                {
-                    is_new_game = false;
-                    break;
-                }
-            }
-            else
-            {
-                is_new_game = current_moves[new_moves_index] != '\0';
-                strcpy(current_moves, moves_ptr == NULL ? "" : moves_ptr);
-                if (moves_ptr != NULL && strlen(moves_ptr) > new_moves_index)
-                {
-                    new_moves_ptr = moves_ptr + new_moves_index;
-                    if (strstr(new_moves_ptr, UCI_DELIMITER) == new_moves_ptr)
-                    {
-                        new_moves_ptr = strlen(new_moves_ptr) > uci_delimiter_static_len ? new_moves_ptr + uci_delimiter_static_len : NULL;
-                    }
-                }
-                break;
-            }
-            new_moves_index++;
-        }
-    }
-    if (is_new_game)
-    {
-        handle_ucinewgame_command(state, token, board);
-        reset_board(board, fen);
-    }
-    if (new_moves_ptr == NULL)
-    {
-        while (board->move_history.count > 0)
-        {
-            undo_move(board, peek_from_move_stack(&(board->move_history)), false);
-        }
-        new_moves_ptr = moves_ptr;
-    }
-    if (new_moves_ptr != NULL)
-    {
-        current_move_ptr = strtok(new_moves_ptr, UCI_DELIMITER);
+        current_move_ptr = strtok(moves_ptr, UCI_DELIMITER);
         reset_move_generation_result(&(generation_result));
         while (current_move_ptr != NULL)
         {
