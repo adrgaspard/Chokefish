7979903339223db76198511206085b4f8abf191e
Gaspard Andrieu <adr.gaspard@gmail.com>
2023-09-18 16:35:13 +0200

    Add search informations UI

---
+++ suite/AdrGaspard.ChokefishSuite.UI.WPF/Converters/Match/MateScoreIntToBrushConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AdrGaspard.ChokefishSuite.UI.WPF.Converters.Match
+{
+    public class MateScoreIntToBrushConverter : IValueConverter
+    {
+        private static readonly SolidColorBrush Mate = new(Color.FromRgb(0x32, 0xDB, 0x4F));
+        private static readonly SolidColorBrush Mated = new(Color.FromRgb(0xF7, 0x2F, 0x2F));
+
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is int score && score != 0 ? score > 0 ? Mate : Mated : (object?)null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ suite/AdrGaspard.ChokefishSuite.UI.WPF/Converters/Match/MateScoreIntToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AdrGaspard.ChokefishSuite.UI.WPF.Converters.Match
+{
+    public class MateScoreIntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is int score && score != 0 ? score > 0 ? $"Mate in {score}" : $"Mated in {score}" : (object)"CONVERTION ERROR";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
